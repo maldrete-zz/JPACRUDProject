9205057590ab137ff7c1859043afec2a2c6090b0
Matt <mra286@humboldt.edu>
2020-01-17 15:14:41 -0700

    create boot MVC project, link JPA dependency

---
+++ Audible/src/main/java/com/skilldistillery/audible/AudibleApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.audible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudibleApplication.class, args);
+	}
+
+}
---
+++ Audible/src/test/java/com/skilldistillery/audible/AudibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.audible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
