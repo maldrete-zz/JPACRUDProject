75be05f046221e537c2bed85205860bfdd928e1c
Matt <mra286@humboldt.edu>
2020-01-20 09:37:36 -0700

    Readme didnt uplaod, built war file

--- Audible/src/main/java/com/skilldistillery/audible/AudibleApplication.java
+++ Audible/src/main/java/com/skilldistillery/audible/AudibleApplication.java
@@ -2,12 +2,18 @@ package com.skilldistillery.audible;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AudibleApplication {
+public class AudibleApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AudibleApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AudibleApplication.class, args);
 	}
-
 }
